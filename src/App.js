da1839f367e9e063960c4ca8196add9a6bcba1d7
EriW <84504738+acu8@users.noreply.github.com>
2023-07-22 22:47:31 +0900

    Button function and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import Button from './Button'
+import { GoBellFill } from "react-icons/go";
+
 
 export default function App() {
+    const handleClick = () => {
+        console.log('click');
+    };
   return (
-    <div>App</div>
+  <>
+    <div>
+        <Button danger onClick={handleClick}><GoBellFill />Click me now!</Button>
+    </div>
+    <div>
+        <Button success>This is button</Button>
+    </div>
+    <div>
+        <Button primary>Hi there!</Button>
+    </div>
+    <div>
+        <Button secondary outline>Hi there!</Button>
+    </div>
+    <div>
+        <Button warning rounded>Hi there!</Button>
+    </div>
+  </>
   )
-}
+};
